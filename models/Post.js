da6672e64cb21c7168825671546e54cffde06ec6
abubakar471 <abdurjoy2001@gmail.com>
2022-04-23 02:53:49 +0600

    users can comment

--- models/Post.js
+++ models/Post.js
@@ -1,12 +1,14 @@
 const mongoose = require('mongoose');
-
-const postSchema = mongoose.Schema({
+const { Schema } = require('mongoose');
+const Comment = require('./Comment');
+const postSchema = new mongoose.Schema({
     title: { type: String, required: true },
     url: { type: String, required: true },
     summary: { type: String, required: true },
-    subreddit : { type : String, required : true} 
-},{
-    timestamps : true
+    subreddit: { type: String, required: true },
+    comments: [{ type: Schema.Types.ObjectId, ref: 'Comment' }]
+}, {
+    timestamps: true
 })
 
 const Post = mongoose.model('posts', postSchema);
--- server.js
+++ server.js
@@ -9,24 +9,28 @@ const PORT = process.env.PORT || 3000;
 const DATABASE = process.env.DATABASE || 'mongodb://localhost:27017/node-reddit-clone';
 require('dotenv').config();
 
-const Post = require('./models/Post');
-
 // database
 mongoose.connect(DATABASE)
     .then(() => console.log('successfully connected to the database'))
     .catch((err) => console.log('database error => %s', err))
+
+// schema models
+const Post = require('./models/Post');
+const Comment = require('./models/Comment');
+
+
 
 app.set('view engine', 'hbs');
 hbs.registerPartials(__dirname + '/views/layouts');
 
 app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended : true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get('/', async(req,res) => {
-    Post.find({}).lean().sort({'createdAt' : -1})
+app.get('/', async (req, res) => {
+    Post.find({}).lean().sort({ 'createdAt': -1 })
         .then((posts) => {
-            res.render('posts_index',{
+            res.render('posts_index', {
                 posts
             })
         })
@@ -34,40 +38,65 @@ app.get('/', async(req,res) => {
             console.log(err.message);
         })
 });
-app.get('/posts/new', (req,res) => {
-    res.render('posts_new',{
-        pageTitle : "New post"
+app.get('/posts/new', (req, res) => {
+    res.render('posts_new', {
+        pageTitle: "New post"
     })
 })
-app.get('/posts/:id', async(req,res) => {
+app.get('/posts/:id', async (req, res) => {
     var url = req.params.id;
-    var post = await Post.findOne({'_id' : url});
-    console.log(post)
-    res.render('posts_show',{
-        pageTitle : post.title,
-        post
-    })
+    Post
+        .findById(req.params.id).lean().populate('comments')
+        .then((post) => {
+            console.log(post);
+            res.render('post_show', { 
+                post 
+            })
+        })
+        .catch((err) => {
+            console.log(err.message);
+        });
 })
-app.get('/n/:subreddit', (req,res) => {
-    Post.find({'subreddit' : req.params.subreddit}).lean()
+app.get('/n/:subreddit', (req, res) => {
+    Post.find({ 'subreddit': req.params.subreddit }).lean()
         .then((posts) => {
-            res.render('posts_index',{
+            res.render('posts_index', {
                 posts
             })
         })
         .catch((err) => console.log(err))
 })
-app.post('/posts/new', (req,res) => {
-    const {title, url, summary, subreddit} = req.body;
-    if(!title || !url || !summary || !subreddit){
-        res.render('posts_new',{
-            pageTitle : "New post",
-            message : "please fill all fields"
+app.post('/posts/new', (req, res) => {
+    const { title, url, summary, subreddit } = req.body;
+    if (!title || !url || !summary || !subreddit) {
+        res.render('posts_new', {
+            pageTitle: "New post",
+            message: "please fill all fields"
         })
     }
-    var newPost = new Post({title, url, summary, subreddit});
+    var newPost = new Post({ title, url, summary, subreddit });
     newPost.save();
     res.redirect('/');
 })
 
-app.listen(PORT, () => console.log('server is running on port %s',PORT));
+
+// CREATE Comment
+app.post('/posts/:postId/comments', (req, res) => {
+    // INSTANTIATE INSTANCE OF MODEL
+    const comment = new Comment(req.body);
+
+    // SAVE INSTANCE OF Comment MODEL TO DB
+    comment
+        .save()
+        .then(() => Post.findById(req.params.postId))
+        .then((post) => {
+            post.comments.unshift(comment);
+            return post.save();
+        })
+        .then(() => res.redirect('/'))
+        .catch((err) => {
+            console.log(err);
+        });
+});
+
+app.listen(PORT, () => console.log('server is running on port %s', PORT));
